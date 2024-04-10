6eeecf87bdddaed12fe3e4f2756cdd4d162359c1
Aleš Roubíček <ales@roubicek.name>
2024-04-10 16:12:26 +0200

    Guard form for well known clients

--- hckr.studio/functions/api/v1/contact-form.js
+++ hckr.studio/functions/api/v1/contact-form.js
@@ -2,7 +2,13 @@
  * @param {EventContext<Env>} context
  */
 export async function onRequestPost({ request, env }) {
-  console.log({ url: request.url, headers: Array.from(request.headers), body: request.body.toString() });
-  const raumeaToken = env.RAUMEA_POSTMARK_TOKEN;
-  return new Response("ok", {status: 200});
+  let url = new URL(request.url);
+  const client = url.searchParams.get("client");
+  const origin = request.headers.get("origin");
+  if (client === "raumea.cz" && origin === "https://www.raumea.cz") {
+    const raumeaToken = env.RAUMEA_POSTMARK_TOKEN;
+    console.log(request.body);
+    return new Response("ok", {status: 200});
+  }
+  return new Response(null, {status: 400});
 }
