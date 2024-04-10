3d9631295387de73f5c478ec77e5843cbeef31c0
Aleš Roubíček <ales@roubicek.name>
2024-04-10 16:29:51 +0200

    Test sending mails

--- hckr.studio/functions/api/v1/contact-form.js
+++ hckr.studio/functions/api/v1/contact-form.js
@@ -1,3 +1,39 @@
+async function sendRaumeaContactForm(token, payload) {
+  const resp = await fetch("https://api.postmarkapp.com/email", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "X-Postmark-Server-Token": token
+    },
+    body: JSON.stringify({
+      "From": "no-reply@hckr.studio",
+      "To": "pavel.trnka@hckr.studio",
+      "Subject": "Kontaktní formulář z webu raumea.cz",
+      "HtmlBody": `
+        <p>Ahoj Michale,</p>
+        <p>Někdo ti vyplnil kontaktní formulář na webu:</p>
+        <dl>
+            <dt>Jméno a příjmení</dt>
+            <dd>${payload.name}</dd>
+            <dt>Společnost</dt>
+            <dd>${payload.company}</dd>
+            <dt>E-mail</dt>
+            <dd>${payload.email}</dd>
+            <dt>Telefon</dt>
+            <dd>${payload.phone}</dd>
+        </dl>
+
+        <blockquote><p>${payload.message}</p></blockquote>
+
+        <p>Tak měj hezký den a hodně štěstí.</p>
+        <p>Tvoje hckr.studio</p>
+      `,
+      "MessageStream": "outbound"
+    })
+  });
+}
+
 /**
  * @param {EventContext<Env>} context
  */
@@ -8,7 +44,7 @@ export async function onRequestPost({ request, env }) {
   if (client === "raumea.cz" && origin === "https://www.raumea.cz") {
     const raumeaToken = env.RAUMEA_POSTMARK_TOKEN;
     const payload = Object.fromEntries(await request.formData());
-    console.log(payload)
+    await sendRaumeaContactForm(raumeaToken, payload);
     return new Response("ok", {status: 200});
   }
   return new Response(null, {status: 400});
