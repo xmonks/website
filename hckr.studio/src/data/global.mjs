596f0b66327ecdac0610261722615332da98a4a8
Aleš Roubíček <ales@roubicek.name>
2024-11-01 11:11:16 +0100

    WIP

--- hckr.studio/src/data/global.mjs
+++ hckr.studio/src/data/global.mjs
@@ -1,5 +1,9 @@
 export default {
   get currentYear() {
     return new Date().getFullYear();
+  },
+  meta: {
+    title: "hckr.studio - extreme. experiments. experience.",
+    description: "We help businesses and individuals succeed in the digital world."
   }
 };
