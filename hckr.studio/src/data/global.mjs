8c03a216b0b1f5eaf6fbcbc4db5bebd8276e3e46
Aleš Roubíček <ales@roubicek.name>
2024-11-09 08:28:04 +0100

    Bumped Blendid

--- hckr.studio/src/data/global.mjs
+++ hckr.studio/src/data/global.mjs
@@ -3,6 +3,7 @@ export default {
     return new Date().getFullYear();
   },
   meta: {
+    lang: "en",
     title: "hckr.studio - extreme. experiments. experience.",
     description: "We help businesses and individuals succeed in the digital world."
   }
