69f6c17aa125e2206e7b1ddc7a687161d698d359
Aleš Roubíček <ales@roubicek.name>
2025-01-07 10:45:20 +0100

    Add UA headers to semantic extractor request

--- workers/hckr-semantic-extractor/src/index.js
+++ workers/hckr-semantic-extractor/src/index.js
@@ -24,7 +24,18 @@ export default {
 
     const url = searchParams.get("url");
     console.log(`Extracting structured data from ${url}`)
-    const resp = await fetch(url);
+    const resp = await fetch(url, {
+      headers: {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "dnt": "1",
+        "sec-ch-ua": `"Chromium";v="131", "Not_A Brand";v="24"`,
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": `"macOS"`,
+      }
+    });
     const html = await resp.text();
     const { document } = parseHTML(html);
     const result = parseStructuredData(document, { base: url });
